80c1eed8ec9a47b8eb03d8ba57e80b3200c0e616
Francesco Pompo <pfrankw@users.noreply.github.com>
2020-10-22 11:43:58 +0200

    Site updated: 2020-10-22 11:43:58

--- js/t.js
+++ js/t.js
@@ -3,8 +3,17 @@ $(document).ready(function() {
         url: 'https://8gi6uzivm6.execute-api.eu-west-1.amazonaws.com/t',
         type: 'POST',
         data: {
-            'uri': window.location.href,
-            
+            uri: window.location.href,
+            bsize: {
+                w: {
+                    h: $(window).height(),
+                    w: $(window).width()
+                },
+                d: {
+                    h: $(document).height(),
+                    w: $(document).width()
+                }
+            }
         },
         success: function(res) {
 
