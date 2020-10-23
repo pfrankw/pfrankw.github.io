6fc5cf303983f23dfea7942dabf1f24bcee57f1e
Francesco Pompo <pfrankw@users.noreply.github.com>
2020-10-23 17:06:06 +0200

    Site updated: 2020-10-23 17:06:06

--- js/t.js
+++ js/t.js
@@ -30,14 +30,15 @@ function makeid(length) {
 }
 
 function sc(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function gc(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    /*for(var i = 0; i
