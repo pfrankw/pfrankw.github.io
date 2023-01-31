9d2e3e1250242c54c19521ac42c53aa6edf06264
Francesco <pfrankw@users.noreply.github.com>
2023-01-31 14:23:54 +0100

    Update

---
+++ discovering_lifes_purpose_through_memento_mori_reflections_on_self_expression/index.md
@@ -0,0 +1,33 @@
+# Discovering Life's Purpose Through Memento Mori: Reflections on Self-Expression
+
+
+
+## Introduction
+
+This year has been a though one. And though times teach great things.
+I am somewhat scared of death, well not that much, but I am starting to respect it, and that's why I am going to write my thoughts here.
+
+
+"**Memento Mori**" would a stoic say.
+
+I always have been a very reserved guy, with all that opsec things in mind, but now the idea of leaving something about me, here on the internet, fascinates me.
+
+
+## Why?
+It is like writing on this blog makes myself **eternal**. I definitely don't want, and I'm not planning to die soon, but in the event of it I would be more "relaxed". I think I understood that the real **scare** of death came from the fact of not having expressed myself "enough" (at all), as I usually never tell things about me to others.
+
+I don't know if it's a thing coming from the **ego** or from the **soul**, but I know it is the wish of leaving something about myself, like the ancients did. I guess I am just (finally) aging, LOL.
+
+This year was deeply different from the others. In the others I have learned a lot of technical skills, but now I feel more **human**, more **vulnerable**. It all started in January with a kind-of love story that completely destroyed me and made me think how much I didn't understand myself and my needs and how I was putting others' ones before mines. To understand this simple concept I needed a person to continuously play with my feelings until I finally reached a **break-point**.
+
+After realizing that I started to see how much people do this every day. I discovered there exist a behavior that comes in two flavors:
+- We keep forgetting about ourselves in order to serve others.
+- We forget about others in order to serve ourselves.
+
+They are the two sides of the same medal, and they can co-exist in the same individual. The medal is the **fear of death**. A binary mind that can only choose one extreme or another, and that cannot decide depending on the situation, is a lazy one.
+
+## The result
+At this point, because I am mortal but I also wanna enjoy life, I decide to **respect death**, but not to fear it, and the only way to enjoy this gift that comes from an unknown place is to always, respectfully, express myself.
+
+Now stop being so serious and lets get back hacking everything :P
+
---
+++ js/t.js
@@ -0,0 +1,89 @@
+function ginfo() {
+    var canvas = document.createElement('canvas');
+    var gl;
+    var debugInfo;
+    var vendor;
+    var renderer;
+
+    try {
+        gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    } catch (e) {}
+
+    if (gl) {
+        debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+        if (!debugInfo)
+            return {};
+        vendor = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+        renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+        return {di: debugInfo, vendor: vendor, renderer: renderer}
+    }
+
+    return {};
+}
+
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+function sc(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function gc(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    for (let x=0; x < ca.length; x++) {
+        var c = ca[x];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+$(document).ready(function() {
+
+    let c = gc('t')
+
+    if (!c || !c.length)
+        sc('t', makeid(10), 30*365)
+
+    let d = {
+        uri: window.location.href,
+        c: gc('t'),
+        r: document.referrer,
+        cpu: navigator.oscpu,
+        platform: navigator.platform,
+        hc: navigator.hardwareConcurrency,
+        dm: navigator.deviceMemory,
+        ginfo: ginfo(),
+        screen: `${screen.width}x${screen.height}x${screen.colorDepth}`,
+        wsize: $(window).width()+'x'+$(window).height(),
+        dsize: $(document).width()+'x'+$(document).height()
+    }
+
+    $.ajax({
+        url: 'https://8gi6uzivm6.execute-api.eu-west-1.amazonaws.com/t',
+        type: 'POST',
+        data: {
+            d: btoa(JSON.stringify(d))
+        },
+        success: function(res) {
+
+        }
+    })
+})
